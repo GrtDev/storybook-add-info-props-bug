856aefb986bc22120b5eca3261375f46223f3c9c
Geert Fokke <gfokke@cartrawler.com>
2018-06-22 15:31:38 +0100

    Remove some comments, Add HTML props for example

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,5 @@
 import { configure, addDecorator } from '@storybook/react'
-import { setDefaults, withInfo } from '@storybook/addon-info'
-
-/**
- * Note that this file is compiled into the build and therefor runs on the `client` side.
- * This means you can not use any NodeJS specific modules here.
- */
+// import { setDefaults, withInfo } from '@storybook/addon-info'
 
 // setDefaults({
 //   header: false,
@@ -16,8 +11,6 @@ import { setDefaults, withInfo } from '@storybook/addon-info'
  */
 // addDecorator((story, context) => withInfo('')(story)(context))
 
-// `app-src` is an alias pointing to the `paths.src`,
-// This is added in the storybook webpack config and allows us to dynamically load the stories of any app.
 const contextRequire = require.context('../src', true, /\.?stories.tsx?$/)
 
 function loadStories() {
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,19 @@
-import React, { SFC } from 'react'
+import React, { SFC, HTMLAttributes } from 'react'
 
 interface IProps {
   /** Children info here */
   children?: React.ReactNode
 }
 
-export const Card: SFC<IProps> = ({ children }) => (
+export const Card: SFC<IProps & HTMLAttributes<HTMLElement>> = ({ children, ...rest }) => (
   <div
     style={{
       textAlign: 'center',
       padding: '20px',
       border: '1px solid #4286f4',
       backgroundColor: '#d6e5ff',
-    }}>
+    }}
+    {...rest}>
     {children}
   </div>
 )
